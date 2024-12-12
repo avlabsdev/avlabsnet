a53f1aa3a9d18c1bdb419f055dda139e673ffa96
Aaron Varga <hello@avlabs.dev>
2024-12-11 21:56:03 -0500

    fontawesome install fix

--- app/src/app/components/Header.tsx
+++ app/src/app/components/Header.tsx
@@ -1,14 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse } from '@awesome.me/kit-4a62263e08/icons/classic/solid';
+import { faBellConcierge, faFlaskRoundPoison, faHouse } from '@awesome.me/kit-4a62263e08/icons/classic/solid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 
 export default function Header() {
     return (
         <header className={"flex flex-row items-center justify-between py-8 container mx-auto"}>
-            <Link href="/" className={"font-medium text-xl flex flex-row items-center gap-2"}>AV Labs</Link>
+            <Link href="/" className={"font-medium text-xl flex flex-row items-center gap-2"}><FontAwesomeIcon icon={faFlaskRoundPoison} className={"text-green-500 text-2xl"} />AV Labs</Link>
             <nav className={"flex flex-row items-center gap-16"}>
-                <Link href="/" className={"flex flex-row items-center gap-2"}><FontAwesomeIcon icon={faHouse} className={"text-red-500"} />Home</Link>
-                <Link href="/services" className={"flex flex-row items-center gap-2"}>Services</Link>
+                <Link href="/" className={"flex flex-row items-center gap-2"}><FontAwesomeIcon icon={faHouse} className={"text-green-500"} />Home</Link>
+                <Link href="/services" className={"flex flex-row items-center gap-2"}><FontAwesomeIcon icon={faBellConcierge} className={"text-green-500"} />Services</Link>
             </nav>
         </header>
     );
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export default function Home() {
   return (
     <>
       <Image src="./undraw_welcome_cats_thqn.svg" width={889.07556} height={459.37952} alt="Kitty cats welcome" className={"mx-auto"} priority></Image>
       <h1>Welcome to my website!</h1>
+      <FontAwesomeIcon icon={faThumbsUp} className="fa-fw" />
+      <FontAwesomeIcon icon={faGithub} className="fa-fw" />
     </>
   );
 }
