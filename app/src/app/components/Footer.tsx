c781d995f5884c812e838a39fc1ee9f4a80dda34
Aaron Varga <hello@avlabs.dev>
2024-12-14 11:47:29 -0500

    icon fix

--- app/src/app/components/Footer.tsx
+++ app/src/app/components/Footer.tsx
@@ -1,5 +1,5 @@
-import { faGithub, faDribbble } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faGithub, faDribbble } from "@fortawesome/free-brands-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 export default function Footer() {
@@ -7,8 +7,8 @@ export default function Footer() {
         <footer className={"py-8 container mx-auto flex flex-row items-center justify-between"}>
             <p>&copy; AV Labs {new Date().getFullYear()}. All rights reserved.</p>
             <span className="flex flex-row gap-16 items-center">
-                <Link href="https://github.com/avlabsdev" target="_blank" className={"flex flex-row items-center gap-2 text-lg"}><FontAwesomeIcon icon={faGithub} className={"text-green-500 text-xl"} />GitHub</Link>
-                <Link href="https://dribbble.com/avlabsdev" target="_blank" className={"flex flex-row items-center gap-2 text-lg"}><FontAwesomeIcon icon={faDribbble} className={"text-green-500 text-xl"} />Dribbble</Link>
+                <Link href="https://github.com/avlabsdev" target="_blank" className={"flex flex-row items-center gap-2 text-lg"}><i className="fa-brands fa-github text-green-500 text-xl"></i>GitHub</Link>
+                <Link href="https://dribbble.com/avlabsdev" target="_blank" className={"flex flex-row items-center gap-2 text-lg"}><i className="fa-brands fa-dribbble text-green-500 text-xl"></i>Dribbble</Link>
             </span>
         </footer>
     );
