5f5cdf09e834fb20df33dde98c4fcce4a4d21638
Aaron Varga <hello@avlabs.dev>
2024-12-14 11:44:24 -0500

    attr fix

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           <p>Everything you need for your next project. User interface design, front-end development, back-end development, and more. Reach out and inquire today.</p>
           <Link href="#/" className="bg-white text-black font-medium text-lg rounded-xl w-fit py-2 px-8">Get started</Link>
         </div>
-        <Image rel="preload" as="image" src="/3d-blocks-blocks-composition-54.png" alt="3d blocks" width="1604" height="1439" className="w-1/2 h-auto" priority />
+        <Image src="/3d-blocks-blocks-composition-54.png" alt="3d blocks" width="1604" height="1439" className="w-1/2 h-auto" priority />
       </div>
     </section>
   );
