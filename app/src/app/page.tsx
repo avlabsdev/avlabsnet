8d5910ba716a23af841cf5fc34854534462a6f39
Aaron Varga <hello@avlabs.dev>
2024-12-14 13:13:10 -0500

    page updates

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -7,12 +7,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <section className="flex flex-col gap-16">
-      <h1 className="sr-only">Home</h1>
-      <div className="flex flex-row gap-8">
-        <div className="flex flex-col gap-4 w-1/2">
-          <h2>Web application software development</h2>
-          <p>Everything you need for your next project. User interface design, front-end development, back-end development, and more. Reach out and inquire today.</p>
-          <Link href="#/" className="bg-white text-black font-medium text-lg rounded-xl w-fit py-2 px-8">Get started</Link>
+      <h1>Home</h1>
+      <div className="flex flex-row gap-16">
+        <div className="flex flex-col gap-8 w-1/2">
+          <div>
+            <h2>Web application software development</h2>
+            <p>Everything you need for your next project. User interface design, front-end development, back-end development, and more.</p>
+          </div>
+          <Link href="#/" className="bg-zinc-900 text-green-500 border border-zinc-800 text-lg rounded-xl w-fit py-3 px-8">Get started</Link>
         </div>
         <Image src="/3d-blocks-blocks-composition-54.png" alt="3d blocks" width="1604" height="1439" className="w-1/2 h-auto" priority />
       </div>
--- app/src/app/services/page.tsx
+++ app/src/app/services/page.tsx
@@ -12,6 +12,8 @@ export default function Services() {
         description: string;
         img_url: string;
         order: number;
+        button_title: string;
+        button_url: string;
     }
 
     const [data, setData] = useState<ServicesItem[]>([]);
@@ -29,15 +31,18 @@ export default function Services() {
     return (
         <section>
             <h1>Services</h1>
-            <div className="flex flex-col gap-64 my-32">
+            <div className="flex flex-col gap-64 my-16">
                 {data.sort((a, b) => a.order - b.order).map((item: ServicesItem) => (
                     <div key={item.id} className="flex flex-row gap-32 items-center">
                         <div className="w-1/2">
                             <Image priority src={item.img_url} alt={item.title} width={500} height={300} className="w-auto" />
                         </div>
-                        <div className="w-1/2">
-                            <h2>{item.title}</h2>
-                            <p>{item.description}</p>
+                        <div className="w-1/2 flex flex-col gap-8">
+                            <div>
+                                <h2>{item.title}</h2>
+                                <p>{item.description}</p>
+                            </div>
+                            <a href={item.button_url} className="bg-zinc-900 text-green-500 border border-zinc-800 text-lg rounded-xl w-fit py-3 px-8">{item.button_title}</a>
                         </div>
                     </div>
                 ))}
